a919ec894df3ebea67fa9a1ca58dd054e566e785
Andreas <andreas.lorentsen@gmail.com>
2023-09-04 02:54:21 +0200

    Create Colors.h

---
+++ Colors.h
@@ -0,0 +1,16 @@
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+};
+
+RGB white = { 255, 255, 255 };
+RGB red = { 255, 0, 0 };
+RGB blue = { 0, 0, 255 };
+RGB yellow = { 244, 229, 28 };
+RGB dullBlue = { 9, 126, 159};
+RGB orange = { 254, 66, 18 };
+RGB lightGrey = { 223, 227, 226 };
+RGB darkGrey = { 66, 67, 66 };
+RGB pink = { 255, 0, 106 };
+RGB off = { 0, 0, 0 };
